25af3ec4ee8db0b62e4f5cc4659c6fbd9475ea19
Deepa <geethapriya.p.b@gmail.com>
2019-09-13 12:39:59 -0700

    linkedlistreverse

--- JavaGit/src/test/LinkedListReverse.java
+++ JavaGit/src/test/LinkedListReverse.java
@@ -2,7 +2,17 @@ package test;
 
 public class LinkedListReverse {
 // create node
-	
+	class Node{
+		
+		int data;
+		Node next;
+		 
+		Node(int data){
+			this.data = data;
+			Node next = null;
+		}
+		
+	}
 	//insert the node
 	
 	
